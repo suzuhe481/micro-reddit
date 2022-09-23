e8dd4eab1b6093e749735d331100b96da3e3441d
Hector <baseballhector@gmail.com>
2022-09-22 20:57:47 -0400

    Creating Comment model and it's columns

---
+++ db/migrate/20220923005553_add_column_to_post.rb
@@ -0,0 +1,5 @@
+class AddColumnToPost < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :posts, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_224301) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_005553) do
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "link"
-    t.string "user_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_224301) do
     t.string "email"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
