46a7780abcb02e9b90afb374201588858e8adaa6
Hector <baseballhector@gmail.com>
2022-09-26 20:02:41 -0400

    Added association for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :user_name, presence: true, length: { in: 4..14 }, uniqueness: true
   validates :email, presence: true
